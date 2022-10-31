b4fe8a0d9198e3ad2021171a168d4465d296db35
sirhmvfx <sirhmvfx@gmail.com>
2022-10-31 02:59:43 +0100

    NodeJs Webserver

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 const http = require('http');
-const fs = require('fs'); 
+const fs = require('fs');
+const path = require('path'); 
 
 const server = http.createServer(function(req, res){
-    fs.readFile('homepage.html', function(err, data){
-        if (err) throw err 
-        res.writeHead(200, {'Content-Type' : 'text/html'});
+    if (req.url === '/'){
+        const filePath = path.join(__dirname, 'public', 'homepage.html')
+        fs.readFile(filePath, 'utf-8', function(err,data){
+            res.writeHead(200, {'Content-Type' : 'text/html'})
+            res.end(data)
+        })
+    }
+        if (req.url === '/homepage.html'){
+        const filePath = path.join(__dirname, 'public', 'homepage.html')
+        fs.readFile(filePath, 'utf-8', function(err,data){
+            res.writeHead(200, {'Content-Type' : 'text/html'})
+            res.end(data)
+        })
+    }
+            if (req.url === '/about.html'){
+        const filePath = path.join(__dirname, 'public', 'about.html')
+        fs.readFile(filePath, 'utf-8', function(err,data){
+            res.writeHead(200, {'Content-Type' : 'text/html'})
+            res.end(data)
+        })
+    }
+            if (req.url === '/contact.html'){
+        const filePath = path.join(__dirname, 'public', 'contact.html')
+        fs.readFile(filePath, 'utf-8', function(err,data){
+            res.writeHead(200, {'Content-Type' : 'text/html'})
+            res.end(data)
+        })
+    }
+})
 
-        res.write(data);
-    });
-});
+const port = 8080; 
 
-server.listen(4000, '127.0.0.1');
+server.listen(port, '127.0.0.1'); 
 
-console.log('Server is connected');
+console.log(`Server is connected to port ${port}`);
